2336ddef9420203b193c3e9467c4621fcb801be8
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2026-01-15 12:43:53 -0600

    Additional test

--- tests/test_english_preset.py
+++ tests/test_english_preset.py
@@ -31,3 +31,21 @@ def test_english_dataset():
     result_2 = censor.apply_censor(input_test_2, matches=matches_2)
 
     assert result_2 == input_test_2
+
+def test_advanced_english():
+    dataset = english_dataset.build()
+    matcher = RegexMatcher(
+        blacklisted_terms=dataset['blacklisted_terms'], 
+        whitelisted_terms=dataset['whitelisted_terms'],
+        blacklist_transformers=english_recommended_blacklist_transformers,
+        whitelist_transformers=english_recommended_whitelist_transformers
+    )
+    censor = FullCensor()
+
+    input_text = "This is a \"fucking\" joke"
+
+    matches = matcher.get_all_matches(input_text)
+    assert len(matches) == 1
+
+    censored = censor.apply_censor(input_text, matches=matches)
+    assert censored == "This is a \"****ing\" joke"
